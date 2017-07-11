a127fd3c6692a1b92a46e90f52654d6515c3fd3e
Laurent Carlier <lordheavym@gmail.com>
2017-07-11 22:14:53 +0200

    initial commit

---
+++ src/RoomComponent.cpp
@@ -0,0 +1,25 @@
+#include "RoomComponent.hpp"
+#include "Mediator.hpp"
+
+namespace isomot
+{
+  
+//--------------------------------------------------------------------------------------------------
+RoomComponent::RoomComponent()
+{
+  this->mediator = 0;
+}
+
+//--------------------------------------------------------------------------------------------------
+void RoomComponent::setMediator(Mediator* mediator)
+{ 
+  this->mediator = mediator;
+}
+
+//--------------------------------------------------------------------------------------------------
+Mediator* RoomComponent::getMediator()
+{ 
+  return mediator;
+}
+
+}
